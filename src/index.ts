f6f06822f216a980bba55588ff64556f52d689fa
yanghao5 <yanghao.buffer@outlook.com>
2025-10-02 16:34:34 +0800

    update 20251002163434

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
+import core = require("@actions/core")
+import github = require("@actions/github")
+
+const name = core.getInput
+
 console.log("Hello Young")
+console.log(`Hello ${name}`)
+
+async function run() {
+    const token = core.getInput('gh-token');
+    const label = core.getInput('label');
+
+    const octokit = github.getOctokit(token);
+    const pullRequest = github.context.payload.pull_request;
+
+    try {
+        if (!pullRequest) {
+            throw new Error('This action can only be run on Pull Requests');
+        }
+
+        await octokit.rest.issues.addLabels({
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            issue_number: pullRequest.number,
+            labels: [label],
+        });
+    } catch (error) {
+        core.setFailed((error as Error)?.message ?? 'Unknown error');
+    }
+}
+
+run();
